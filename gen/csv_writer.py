6e92c5936823340ee9dcf1116c9ec43df06658d3
chawki benchehida <chawki27000@hotmail.com>
2018-12-21 17:29:25 +0100

    make a separate file between simulation and analysis

--- gen/csv_writer.py
+++ gen/csv_writer.py
@@ -2,8 +2,9 @@ import csv
 
 
 class CSVWriter:
-    def __init__(self, messages_i):
+    def __init__(self, messages_i, type):
         self.messages_i = messages_i
+        self.type = type
 
     def flit_print(self):
         for message in self.messages_i:
@@ -16,9 +17,6 @@ class CSVWriter:
         with open(link, mode='w') as file:
             writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-            # File Header
-            writer.writerow(['i', 'WCLA', 'L_1', 'L_2', 'L_3', 'L_4'])
-
             # Array to dictionary
             dico = dict()
 
@@ -30,30 +28,39 @@ class CSVWriter:
                     dico[message.id].append(message)
 
             # generation
-            for item in dico.items():
-                instance = item[1]
-                if len(instance) == 1:
-                    writer.writerow([instance[0].id,
-                                     instance[0].get_analysis_latency(),
-                                     instance[0].get_latency()])
+            if self.type == 0:  # Simulation
+                # File Header
+                writer.writerow(['i', 'L_1', 'L_2', 'L_3', 'L_4'])
 
-                if len(instance) == 2:
-                    writer.writerow([instance[0].id,
-                                     instance[0].get_analysis_latency(),
-                                     instance[0].get_latency(),
-                                     instance[1].get_latency()])
+                for item in dico.items():
+                    instance = item[1]
 
-                if len(instance) == 3:
-                    writer.writerow([instance[0].id,
-                                     instance[0].get_analysis_latency(),
-                                     instance[0].get_latency(),
-                                     instance[1].get_latency(),
-                                     instance[2].get_latency()])
+                    if len(instance) == 1:
+                        writer.writerow([instance[0].id,
+                                         instance[0].get_latency()])
 
-                if len(instance) == 4:
-                    writer.writerow([instance[0].id,
-                                     instance[0].get_analysis_latency(),
-                                     instance[0].get_latency(),
-                                     instance[1].get_latency(),
-                                     instance[2].get_latency(),
-                                     instance[3].get_latency()])
+                    if len(instance) == 2:
+                        writer.writerow([instance[0].id,
+                                         instance[0].get_latency(),
+                                         instance[1].get_latency()])
+
+                    if len(instance) == 3:
+                        writer.writerow([instance[0].id,
+                                         instance[0].get_latency(),
+                                         instance[1].get_latency(),
+                                         instance[2].get_latency()])
+
+                    if len(instance) == 4:
+                        writer.writerow([instance[0].id,
+                                         instance[0].get_latency(),
+                                         instance[1].get_latency(),
+                                         instance[2].get_latency(),
+                                         instance[3].get_latency()])
+
+            elif self.type == 1:  # Analysis
+                # File Header
+                writer.writerow(['i', 'WCLA'])
+
+                for message in self.messages_i:
+                    writer.writerow([message.id,
+                                     message.get_analysis_latency()])
---
+++ simulation.py
@@ -0,0 +1,87 @@
+import logging
+import sys
+import getopt
+import os
+
+from architecture.noc import NoC
+from engine.simulation import Simulation
+from gen.generation import Generation
+from gen.csv_writer import CSVWriter
+
+
+def main():
+    # parse input files
+    input_files = os.popen("ls input").read().split('\n')
+    del input_files[-1]
+
+    # CLI Argument parsing
+    level = 0
+    try:
+        options, args = getopt.getopt(sys.argv[1:], 'dimu:')
+    except getopt.error as msg:
+        sys.stdout = sys.stderr
+        print(msg)
+        print("""usage: %s [-d|-i] [-u|-m|-]
+                -d, -i: DEBUG / INFO """ % sys.argv[0])
+        sys.exit()
+
+    for opt, value in options:
+        if opt in '-d':
+            level = logging.DEBUG
+        if opt in '-i':
+            level = logging.INFO
+
+    # file parsing loop
+    for file in input_files:
+        Simulation.CLOCK = 0
+
+        # NoC Settings
+        generation = Generation()
+        generation.config('input/' + file + '/config.yml')
+
+        # Messages generation
+        messages = generation.scenario('input/' + file + '/scenario.yml')
+
+        square_size = generation.square_size()
+        nbvc = generation.nbvc()
+        vc_size = generation.vc_size()
+        vc_quantum = generation.vc_quantum()
+        arbitration = generation.arbitration()
+
+        noc = NoC('Network-On-Chip', square_size, nbvc, vc_size, vc_quantum)
+
+        generation.set_noc(noc)
+
+        logging.basicConfig(level=level)
+
+        logging.info('###################################################################')
+        logging.info('### ReTiNAS - Real-Time Network-on-chip Analysis and Simulation ###')
+        logging.info('------ NoC Configuration ------')
+        logging.info('\tDimension : %d x %d' % (square_size, square_size))
+        logging.info('\tVC Number per Input : %d' % nbvc)
+        logging.info('\tVC Buffer size : %d' % vc_size)
+        logging.info('\tVC Quantum setting : %s' % vc_quantum)
+        logging.info('\tArbitration Policy : %s' % arbitration)
+        logging.info('-------------------------------')
+
+        # Simulator Settings
+        simulation = Simulation(noc, generation.hyperperiod())
+
+        for message in messages:
+            simulation.send_message(message)
+
+        # Starting Simulation
+        logging.info('### Simulation --> START - hyperperiod : %d ###' % generation.hyperperiod())
+        simulation.simulate(arbitration)
+        logging.info('### Simulation --> END ###')
+
+        # printing
+        messages_i = simulation.get_message_instance_tab()
+        csv = CSVWriter(messages_i, 0)
+        csv.generate_csv('input/' + file + '/result_sim.csv')
+
+        simulation.reset_clock()
+
+
+if __name__ == "__main__":
+    main()
